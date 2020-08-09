7a2dd81f8479d4abb72d07836529325244c9127e
petrnalevka <petr@nalevka.com>
2020-08-09 21:15:58 +0200

    Looking for the differences..

--- app/src/main/java/com/urbandroid/recordforeground/RecordingReceiver.kt
+++ app/src/main/java/com/urbandroid/recordforeground/RecordingReceiver.kt
@@ -4,15 +4,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import java.util.*
 
 class RecordingReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.i(TAG, "Broadcast received")
         context?.apply {
-            Log.i(TAG , "Foreground service will start at ${Date(System.currentTimeMillis() + 60000)}")
-            RecordingService.scheduleStart(this, 1)
+            RecordingService.startService(this)
         }
     }
 }
